0ac485a669fd757819aebd739aa7bcf182dca958
ipulkitsingla <156352781+ipulkitsingla@users.noreply.github.com>
2025-03-22 16:29:01 +0530

    ..

--- Client/vite.config.js
+++ Client/vite.config.js
@@ -1,7 +1,29 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist',
+    sourcemap: false,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom', 'react-router-dom'],
+          mui: ['@mui/material', '@mui/icons-material', '@mui/styles'],
+          slider: ['react-slick', 'slick-carousel']
+        }
+      }
+    }
+  },
+  resolve: {
+    alias: {
+      '@': '/src'
+    }
+  },
+  server: {
+    port: 3000,
+    open: true
+  }
 })
