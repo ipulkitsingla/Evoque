319e4b60d660c2283315ff12f262cab9ff1c1c2c
ipulkitsingla <156352781+ipulkitsingla@users.noreply.github.com>
2025-03-22 16:47:15 +0530

    ..

--- Client/vite.config.js
+++ Client/vite.config.js
@@ -11,19 +11,15 @@ export default defineConfig({
       output: {
         manualChunks: {
           vendor: ['react', 'react-dom', 'react-router-dom'],
-          mui: ['@mui/material', '@mui/icons-material', '@mui/styles'],
+          mui: ['@mui/material', '@mui/icons-material', '@mui/styles', '@mui/lab', '@mui/system'],
+          form: ['formik', 'yup', 'react-select'],
           slider: ['react-slick', 'slick-carousel', 'swiper', 'react-range-slider-input'],
-          bootstrap: ['bootstrap', 'react-bootstrap']
+          image: ['react-inner-image-zoom', 'react-image-magnify', 'react-lazy-load-image-component'],
+          ui: ['react-loading-skeleton', 'react-hot-toast', 'react-toastify'],
+          bootstrap: ['bootstrap', 'react-bootstrap'],
+          utils: ['moment', 'jwt-decode', 'axios']
         }
-      },
-      commonjsOptions: {
-        include: [/node_modules/],
-        transformMixedEsModules: true
       }
-    },
-    commonjsOptions: {
-      include: [/node_modules/],
-      transformMixedEsModules: true
     }
   },
   css: {
@@ -31,6 +27,9 @@ export default defineConfig({
       scss: {
         quietDeps: true
       }
+    },
+    modules: {
+      localsConvention: 'camelCase'
     }
   },
   optimizeDeps: {
@@ -39,13 +38,34 @@ export default defineConfig({
       'bootstrap',
       'swiper/react',
       'swiper/css',
-      'react-range-slider-input'
-    ]
+      'react-range-slider-input',
+      'react-slick',
+      'slick-carousel',
+      'react-inner-image-zoom',
+      'react-image-magnify',
+      'react-lazy-load-image-component',
+      'react-loading-skeleton',
+      'react-hot-toast',
+      'react-toastify',
+      'formik',
+      'yup',
+      'react-select',
+      'moment',
+      'jwt-decode'
+    ],
+    esbuildOptions: {
+      target: 'es2020'
+    }
   },
   resolve: {
     alias: {
       '@': '/src'
-    }
+    },
+    extensions: ['.js', '.jsx', '.json']
+  },
+  esbuild: {
+    loader: 'jsx',
+    target: 'es2020'
   },
   server: {
     port: 3000,
